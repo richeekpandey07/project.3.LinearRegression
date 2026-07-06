73763ac8cd21f6e20b50a185ae1c12a82f70791d
richeekpandey07 <richeekpandey@gmail.com>
2026-07-06 19:41:19 +0530

    Comment out page configuration and title
    Commented out page configuration and title sections.

--- project_3.py
+++ project_3.py
@@ -14,29 +14,15 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 
 # --------------------------------------------------
-# Page Configuration
-# --------------------------------------------------
-st.set_page_config(
-    page_title="Canada Per Capita Income Prediction",
-    page_icon="📈",
-    layout="wide"
-)
+# # Page Configuration
+# # --------------------------------------------------
+# st.set_page_config(
+#     page_title="Canada Per Capita Income Prediction",
+#     page_icon="📈",
+#     layout="wide"
+# )
 
-# --------------------------------------------------
-# Title
-# --------------------------------------------------
-st.title("📈 Canada Per Capita Income Prediction")
-st.markdown(
-    """
-This app uses **Linear Regression** to predict the **Per Capita Income (US$)** of Canada
-based on historical data.
-"""
-)
 
-# --------------------------------------------------
-# Load Dataset
-# --------------------------------------------------
-df = pd.read_csv("canada_per_capita_income.csv")
 st.markdown("""
 <div style="
 padding:35px;
